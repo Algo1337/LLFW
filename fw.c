d1a57356899c237ab7e74231460e2a2854d0cd38
Algo <142917637+Algo1337@users.noreply.github.com>
2026-05-01 00:09:36 -0500

    cli

--- fw.c
+++ fw.c
@@ -1,6 +1,6 @@
 #include "src/init.h"
 
-string args[50 * 8];
+string args[100 * 8];
 
 public string HELP_LIST = "   Name           Description\r\n"
 "______________________________________\r\n"
@@ -8,9 +8,11 @@ public string HELP_LIST = "   Name           Description\r\n"
 "   --myip         You're IP to whitlist\r\n"
 "   --debug        Enable debug mode\r\n";
 
+public fn term_cli();
+
 public int entry(int argc, string argv[])
 {
-    mem_cpy(args, argv, argc * 8);
+    mem_cpy(args, argv, 8 * argc);
     uninit_mem();
     set_heap_sz(526870912);
     init_mem();
@@ -18,17 +20,38 @@ public int entry(int argc, string argv[])
     toggle_protection(fw);
 
     int pos = 0;
-    if(pos = array_contains_str((array)args, "--debug"))
-        toggle_debug_mode();
-        
-    if(pos = array_contains_str((array)args, "--serv_ip"))
+    // if(array_contains_str((array)args, "--debug"))
+    //     toggle_debug_mode();
+
+    if((pos = array_contains_str((array)args, "--serv_ip")) > -1)
         fw->system_ip = str_dup(args[pos + 1]);
 
-    if(pos = array_contains_str((array)args, "--myip"))
+    if((pos = array_contains_str((array)args, "--myip")) > -1)
         whitlist_ip(fw, args[pos + 1]);
 
     _printf("Socket: %d\n", (void *)&fw->socket->fd);
 
-    monitor(fw);
+    thread t = create_thread((handler_t)monitor, fw, 0);
+    start_thread(t);
+
+    term_cli();
     return 0;
+}
+
+public fn term_cli()
+{
+    char INPUT[1024] = {0};
+    while(1)
+    {
+        print("> ");
+        int bytes = get_input(INPUT, 1023);
+        if(bytes <= 0)
+            continue;
+
+        if(str_cmp(INPUT, "help")) {
+            println("working");
+        } else if(str_cmp(INPUT, "geo")) {
+            println("Working 2");
+        }
+    }
 }
